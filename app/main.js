ad5a624f3140e58a1e994f2008f81eed4446b995
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-02-26 16:45:10 +0200

    Fuel consumption calculation

---
+++ app/main.js
@@ -0,0 +1,35 @@
+function CLEANCalculateFuelAndCost(distance, fuelConsumption, fuelPrice) {
+    let fuelAmount = Math.abs(distance * fuelConsumption / 100);
+    let fuelCost = Math.abs(fuelAmount * fuelPrice);
+    return {
+        fuelAmount: fuelAmount.toFixed(2),
+        fuelCost: fuelCost.toFixed(2)
+    }
+}
+
+function resultFunc() {
+    const resultSection = document.getElementById('result-section');
+    const wrongInput = document.getElementById('wrong-input-section');
+
+    const distanceInput = document.getElementById('distance');
+    const fuelConsumptionInput = document.getElementById('consumption');
+    const fuelPriceInput = document.getElementById('fuel-price');
+
+    if (distanceInput.value && fuelConsumptionInput.value && fuelPriceInput.value){
+        const distance = parseFloat(distanceInput.value);
+        const fuelConsumption = parseFloat(fuelConsumptionInput.value);
+        const fuelPrice = parseFloat(fuelPriceInput.value);
+    
+        let result = CLEANCalculateFuelAndCost(distance, fuelConsumption, fuelPrice);
+    
+        document.getElementById('fuel-amount').textContent = result.fuelAmount + ' liters';
+        document.getElementById('fuel-cost').textContent = result.fuelCost + '  EUR';
+        
+        resultSection.style.display = 'block';
+        wrongInput.style.display = 'none';
+    } else {
+        resultSection.style.display = 'none';
+        wrongInput.style.display = 'block';
+    }
+}
+
