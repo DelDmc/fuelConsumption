ae664d8e31f712ddb08049534377a725eb9457df
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-03-07 22:44:56 +0200

    object short notation used

--- app/fuelConsCostScript/consumptionCostCalculation.js
+++ app/fuelConsCostScript/consumptionCostCalculation.js
@@ -1,6 +1,6 @@
 function calculateFuelAndCost(tripParametersObject) {
     let fuelAmount = Math.abs(tripParametersObject.distance * tripParametersObject.fuelConsumption / 100);
-    let fuelCost = Math.abs(tripParametersObject.fuelAmount * tripParametersObject.fuelPrice);
+    let fuelCost = Math.abs(fuelAmount * tripParametersObject.fuelPrice);
     return {
         fuelAmount: fuelAmount.toFixed(2),
         fuelCost: fuelCost.toFixed(2)
