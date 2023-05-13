73d96fc04f2f6e87fdf8589d4aa210730f47b05d
K.S <del12dmc@gmail.com>
2023-05-13 19:24:04 +0300

    changes as per eslint

--- app/fuelConsCostScript/consumptionCostCalculation.js
+++ app/fuelConsCostScript/consumptionCostCalculation.js
@@ -1,8 +1,21 @@
-function calculateFuelAndCost(tripParametersObject) {
-    let fuelAmount = Math.abs(tripParametersObject.distance * tripParametersObject.fuelConsumption / 100);
-    let fuelCost = Math.abs(fuelAmount * tripParametersObject.fuelPrice);
-    return {
-        fuelAmount: fuelAmount.toFixed(2),
-        fuelCost: fuelCost.toFixed(2)
-    }
+/**
+ * Calculates the fuel amount and cost for a given trip based on the trip parameters.
+ * @param {Object} tripParameters - The parameters of the trip.
+ * @param {number} tripParameters.distance - The distance of the trip.
+ * @param {number} tripParameters.fuelConsumption - The fuel consumption per 100 km.
+ * @param {number} tripParameters.fuelPrice - The price of fuel per liter.
+ * @return {Object} The calculated fuel amount and cost.
+ * @property {string} fuelAmount - The calculated fuel amount rounded to 2 decimal places.
+ * @property {string} fuelCost - The calculated fuel cost rounded to 2 decimal places.
+ */
+function calculateFuelAndCost(tripParameters) {
+  const fuelAmount = Math.abs(
+      tripParameters.distance * tripParameters.fuelConsumption / 100,
+  );
+  const fuelCost = Math.abs(fuelAmount * tripParameters.fuelPrice);
+  return {
+    fuelAmount: fuelAmount.toFixed(2),
+    fuelCost: fuelCost.toFixed(2),
+  };
 }
+
--- app/main.js
+++ app/main.js
@@ -1,26 +1,66 @@
+/**
+ * Performs calculations based on user input and displays the result in the DOM.
+ * @return {void}
+ */
 function resultFunc() {
-    const resultSection = document.getElementById('result-section');
-    const wrongInput = document.getElementById('wrong-input-section');
+  /**
+     * @type {HTMLElement} - The result section element.
+     */
+  const resultSection = document.getElementById('result-section');
 
-    const distanceInput = document.getElementById('distance');
-    const fuelConsumptionInput = document.getElementById('consumption');
-    const fuelPriceInput = document.getElementById('fuel-price');
+  /**
+     * @type {HTMLElement} - The wrong input section element.
+     */
+  const wrongInput = document.getElementById('wrong-input-section');
 
-    if (distanceInput.value && fuelConsumptionInput.value && fuelPriceInput.value){
-        const distance = parseFloat(distanceInput.value);
-        const fuelConsumption = parseFloat(fuelConsumptionInput.value);
-        const fuelPrice = parseFloat(fuelPriceInput.value);
-    
-        let result = calculateFuelAndCost({distance, fuelConsumption, fuelPrice});
-    
-        document.getElementById('fuel-amount').textContent = result.fuelAmount + ' liters';
-        document.getElementById('fuel-cost').textContent = result.fuelCost + '  EUR';
-        
-        resultSection.style.display = 'block';
-        wrongInput.style.display = 'none';
-    } else {
-        resultSection.style.display = 'none';
-        wrongInput.style.display = 'block';
-    }
+  /**
+     * @type {HTMLInputElement} - The input field for distance.
+     */
+  const distanceInput = document.getElementById('distance');
+
+  /**
+     * @type {HTMLInputElement} - The input field for fuel consumption.
+     */
+  const fuelConsumptionInput = document.getElementById('consumption');
+
+  /**
+     * @type {HTMLInputElement} - The input field for fuel price.
+     */
+  const fuelPriceInput = document.getElementById('fuel-price');
+
+  // eslint-disable-next-line max-len
+  if (distanceInput.value && fuelConsumptionInput.value && fuelPriceInput.value) {
+    /**
+       * @type {number} - The distance entered by the user.
+       */
+    const distance = parseFloat(distanceInput.value);
+
+    /**
+       * @type {number} - The fuel consumption entered by the user.
+       */
+    const fuelConsumption = parseFloat(fuelConsumptionInput.value);
+
+    /**
+       * @type {number} - The fuel price entered by the user.
+       */
+    const fuelPrice = parseFloat(fuelPriceInput.value);
+
+    /**
+       * @type {Object} - The result of the fuel and cost calculation.
+       * @property {number} fuelAmount - The calculated fuel amount.
+       * @property {number} fuelCost - The calculated fuel cost.
+       */
+    const result = calculateFuelAndCost({distance, fuelConsumption, fuelPrice});
+
+    document.getElementById('fuel-amount')
+        .textContent = result.fuelAmount + ' liters';
+    document.getElementById('fuel-cost')
+        .textContent = result.fuelCost + ' EUR';
+
+    resultSection.style.display = 'block';
+    wrongInput.style.display = 'none';
+  } else {
+    resultSection.style.display = 'none';
+    wrongInput.style.display = 'block';
+  }
 }
-
